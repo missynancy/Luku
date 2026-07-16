d0baa65815fd84c82f817118c4056864a137a5ff
Nancy Wambui <missynancy722@outlook.com>
2026-07-16 18:16:41 +0300

    fix astro config base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
 export default defineConfig({
-  site: 'https://smartlukufitness.com',
+  site: 'https://missynancy.github.io',
+  base: '/Luku',
   integrations: [preact()]
 });

49876daeddd411603390d0591b2b6a3e8e85dfdb
Nancy Wambui <missynancy722@outlook.com>
2026-07-17 10:19:47 +0300

    fix robots.txt domain

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,5 +1,5 @@
 export const GET = () => {
-  return new Response(`User-agent: *\nAllow: /\nSitemap: https://smartlukufitness.com/sitemap-index.xml\n`, {
+  return new Response(`User-agent: *\nAllow: /\nSitemap: https://missynancy.github.io/Luku/sitemap-index.xml\n`, {
     headers: { 'Content-Type': 'text/plain; charset=utf-8' }
   });
 };

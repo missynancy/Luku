c59e3af5335412a261804a8efc396d16ace3d673
Nancy Wambui <missynancy722@outlook.com>
2026-07-17 10:06:54 +0300

    fix sitemap domain, remove conflicting sitemap package

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import preact from '@astrojs/preact';
 export default defineConfig({
   site: 'https://missynancy.github.io',
   base: '/Luku',
-  integrations: [preact()]
+  integrations: [preact()],
 });
